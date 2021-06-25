48651a4ae6547335976a6e1e387e82c8a8631d9c
ZZX <2604533953@qq.com>
2021-06-25 18:39:15 +0800

    Add files via upload

---
+++ lib/encoders.py
@@ -0,0 +1,316 @@
+"""VSE modules"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+from collections import OrderedDict
+
+from transformers import BertModel
+
+from lib.modules.resnet import ResnetFeatureExtractor
+from lib.modules.aggr.gpo import GPO
+from lib.modules.mlp import MLP
+
+import logging
+
+import math
+
+logger = logging.getLogger(__name__)
+
+
+class Rs_GCN(nn.Module):
+
+    def __init__(self, in_channels, inter_channels, bn_layer=True):
+        super(Rs_GCN, self).__init__()
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+
+        conv_nd = nn.Conv1d
+        max_pool = nn.MaxPool1d
+        bn = nn.BatchNorm1d
+
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                         kernel_size=1, stride=1, padding=0)
+
+        if bn_layer:
+            self.W = nn.Sequential(
+                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                        kernel_size=1, stride=1, padding=0),
+                bn(self.in_channels)
+            )
+            nn.init.constant(self.W[1].weight, 0)
+            nn.init.constant(self.W[1].bias, 0)
+        else:
+            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                             kernel_size=1, stride=1, padding=0)
+            nn.init.constant(self.W.weight, 0)
+            nn.init.constant(self.W.bias, 0)
+
+        self.theta = None
+        self.phi = None
+
+
+        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+
+
+
+    def forward(self, v):
+        '''
+        :param v: (B, D, N)
+        :return:
+        '''
+        batch_size = v.size(0)
+
+        g_v = self.g(v).view(batch_size, self.inter_channels, -1)
+        g_v = g_v.permute(0, 2, 1)
+
+        theta_v = self.theta(v).view(batch_size, self.inter_channels, -1)
+        theta_v = theta_v.permute(0, 2, 1)
+        phi_v = self.phi(v).view(batch_size, self.inter_channels, -1)
+        R = torch.matmul(theta_v, phi_v)
+        N = R.size(-1)
+        R_div_C = R / N
+
+        y = torch.matmul(R_div_C, g_v)
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *v.size()[2:])
+        W_y = self.W(y)
+        v_star = W_y + v
+
+        return v_star
+
+def l1norm(X, dim, eps=1e-8):
+    """L1-normalize columns of X
+    """
+    norm = torch.abs(X).sum(dim=dim, keepdim=True) + eps
+    X = torch.div(X, norm)
+    return X
+
+
+def l2norm(X, dim, eps=1e-8):
+    """L2-normalize columns of X
+    """
+    norm = torch.pow(X, 2).sum(dim=dim, keepdim=True).sqrt() + eps
+    X = torch.div(X, norm)
+    return X
+
+
+def l2norm_2(X):
+    """L2-normalize columns of X
+    """
+    norm = torch.pow(X, 2).sum(dim=1, keepdim=True).sqrt()
+    X = torch.div(X, norm)
+    return X
+
+
+def maxk_pool1d_var(x, dim, k, lengths):
+    results = list()
+    lengths = list(lengths.cpu().numpy())
+    lengths = [int(x) for x in lengths]
+    for idx, length in enumerate(lengths):
+        k = min(k, length)
+        max_k_i = maxk(x[idx, :length, :], dim - 1, k).mean(dim - 1)
+        results.append(max_k_i)
+    results = torch.stack(results, dim=0)
+    return results
+
+
+def maxk_pool1d(x, dim, k):
+    max_k = maxk(x, dim, k)
+    return max_k.mean(dim)
+
+
+def maxk(x, dim, k):
+    index = x.topk(k, dim=dim)[1]
+    return x.gather(dim, index)
+
+
+def get_text_encoder(embed_size, no_txtnorm=False):
+    return EncoderText(embed_size, no_txtnorm=no_txtnorm)
+
+
+def get_image_encoder(data_name, img_dim, embed_size, precomp_enc_type='basic',
+                      backbone_source=None, backbone_path=None, no_imgnorm=False):
+    """A wrapper to image encoders. Chooses between an different encoders
+    that uses precomputed image features.
+    """
+    if precomp_enc_type == 'basic':
+        img_enc = EncoderImageAggr(
+            img_dim, embed_size, precomp_enc_type, no_imgnorm)
+    elif precomp_enc_type == 'backbone':
+        backbone_cnn = ResnetFeatureExtractor(backbone_source, backbone_path, fixed_blocks=2)
+        img_enc = EncoderImageFull(backbone_cnn, img_dim, embed_size, precomp_enc_type, no_imgnorm)
+    else:
+        raise ValueError("Unknown precomp_enc_type: {}".format(precomp_enc_type))
+
+    return img_enc
+
+
+class EncoderImageAggr(nn.Module):
+    def __init__(self, img_dim, embed_size, precomp_enc_type='basic', no_imgnorm=False):
+        super(EncoderImageAggr, self).__init__()
+        self.embed_size = embed_size
+        self.no_imgnorm = no_imgnorm
+        self.fc = nn.Linear(img_dim, embed_size)
+        self.precomp_enc_type = precomp_enc_type
+        if precomp_enc_type == 'basic':
+            self.mlp = MLP(img_dim, embed_size // 2, embed_size, 2)
+        self.gpool = GPO(32, 32)
+        # GCN reasoning 
+        self.Rs_GCN_1 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_2 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_3 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_4 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+
+        self.init_weights()
+
+    def init_weights(self):
+        """Xavier initialization for the fully connected layer
+        """
+        r = np.sqrt(6.) / np.sqrt(self.fc.in_features +
+                                  self.fc.out_features)
+        self.fc.weight.data.uniform_(-r, r)
+        self.fc.bias.data.fill_(0)
+
+    def forward(self, images, image_lengths):
+        """Extract image feature vectors."""
+        features = self.fc(images)
+        if self.precomp_enc_type == 'basic':
+            # When using pre-extracted region features, add an extra MLP for the embedding transformation
+            features = self.mlp(images) + features
+            #print("if self.precomp_enc_type == 'basic':")
+            #kk = input()
+        #print(features.size())
+        GCN_img_emd = features.permute(0, 2, 1)
+        GCN_img_emd = self.Rs_GCN_1(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_2(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_3(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_4(GCN_img_emd)
+        # -> B,N,D
+        features = GCN_img_emd.permute(0, 2, 1)
+
+        features, pool_weights = self.gpool(features, image_lengths)
+
+        if not self.no_imgnorm:
+            features = l2norm(features, dim=-1)
+
+        return features
+
+class EncoderImageFull(nn.Module):
+    def __init__(self, backbone_cnn, img_dim, embed_size, precomp_enc_type='basic', no_imgnorm=False):
+        super(EncoderImageFull, self).__init__()
+        self.backbone = backbone_cnn
+        self.image_encoder = EncoderImageAggr(img_dim, embed_size, precomp_enc_type, no_imgnorm)
+        self.backbone_freezed = False
+
+    def forward(self, images):
+        """Extract image feature vectors."""
+        base_features = self.backbone(images)
+
+        if self.training:
+            # Size Augmentation during training, randomly drop grids
+            base_length = base_features.size(1)
+            features = []
+            feat_lengths = []
+            rand_list_1 = np.random.rand(base_features.size(0), base_features.size(1))
+            rand_list_2 = np.random.rand(base_features.size(0))
+            for i in range(base_features.size(0)):
+                if rand_list_2[i] > 0.2:
+                    feat_i = base_features[i][np.where(rand_list_1[i] > 0.20 * rand_list_2[i])]
+                    len_i = len(feat_i)
+                    pads_i = torch.zeros(base_length - len_i, base_features.size(-1)).to(base_features.device)
+                    feat_i = torch.cat([feat_i, pads_i], dim=0)
+                else:
+                    feat_i = base_features[i]
+                    len_i = base_length
+                feat_lengths.append(len_i)
+                features.append(feat_i)
+            base_features = torch.stack(features, dim=0)
+            base_features = base_features[:, :max(feat_lengths), :]
+            feat_lengths = torch.tensor(feat_lengths).to(base_features.device)
+        else:
+            feat_lengths = torch.zeros(base_features.size(0)).to(base_features.device)
+            feat_lengths[:] = base_features.size(1)
+
+        features = self.image_encoder(base_features, feat_lengths)
+
+        return features
+
+    def freeze_backbone(self):
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+        logger.info('Backbone freezed.')
+
+    def unfreeze_backbone(self, fixed_blocks):
+        for param in self.backbone.parameters():  # open up all params first, then adjust the base parameters
+            param.requires_grad = True
+        self.backbone.set_fixed_blocks(fixed_blocks)
+        self.backbone.unfreeze_base()
+        logger.info('Backbone unfreezed, fixed blocks {}'.format(self.backbone.get_fixed_blocks()))
+
+
+# Language Model with BERT
+class EncoderText(nn.Module):
+    def __init__(self, embed_size, no_txtnorm=False):
+        super(EncoderText, self).__init__()
+        self.embed_size = embed_size
+        self.no_txtnorm = no_txtnorm
+
+        self.bert = BertModel.from_pretrained('/public/ZZX/SCAN_dataset/vse_infty-master/bert-base-uncased')
+        #self.linear = nn.Linear(768, embed_size)
+        self.gpool = GPO(32, 32)
+
+        self.rnn = nn.GRU(768, embed_size, 1, batch_first=True, bidirectional=True)
+        # GCN reasoning 
+        self.Rs_GCN_1 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_2 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_3 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+        self.Rs_GCN_4 = Rs_GCN(in_channels=embed_size, inter_channels=embed_size)
+
+    def forward(self, x, lengths):
+        """Handles variable size captions
+        """
+        # Embed word ids to vectors
+        bert_attention_mask = (x != 0).float()
+        bert_emb = self.bert(x, bert_attention_mask)[0]  # B x N x D
+        cap_len = lengths
+
+        #cap_emb = self.linear(bert_emb)
+        #rnn_img, hidden_state = self.img_rnn(GCN_img_emd)
+        cap_emb, hidden_state = self.rnn(bert_emb)
+        #if self.data_name != 'f30k_precomp':
+        #    fc_img_emd = l2norm_2(fc_img_emd)
+
+        #print(cap_emb.size())
+
+        GCN_img_emd = cap_emb.permute(0, 2, 1)
+        GCN_img_emd = self.Rs_GCN_1(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_2(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_3(GCN_img_emd)
+        GCN_img_emd = self.Rs_GCN_4(GCN_img_emd)
+        # -> B,N,D
+        cap_emb = GCN_img_emd.permute(0, 2, 1)
+
+        #GCN_img_emd = l2norm(GCN_img_emd)
+
+        #rnn_img, hidden_state = self.img_rnn(GCN_img_emd)
+
+
+        pooled_features, pool_weights = self.gpool(cap_emb, cap_len.to(cap_emb.device))
+
+        # normalization in the joint embedding space
+        if not self.no_txtnorm:
+            pooled_features = l2norm(pooled_features, dim=-1)
+
+        return pooled_features
